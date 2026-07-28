8ad5a32a8908bda068386b0574f8ecae4fe1bf08
monazarea <monazarea3@gmail.com>
2026-07-28 17:37:59 -0400

    feat(onboarding): add onboarding flow with dynamic swiping cards and navigation

--- Carely/App/AppState.swift
+++ Carely/App/AppState.swift
@@ -10,6 +10,8 @@ import Combine
 import Foundation
 
 enum AppFlow: Equatable {
+    case splash
+    case onboarding
     case auth
     case profileSetupDecision
     case profileSetup
@@ -18,29 +20,45 @@ enum AppFlow: Equatable {
 
 @MainActor
 final class AppState: ObservableObject {
-    @Published private(set) var flow: AppFlow
-
+    @Published private(set) var flow: AppFlow = .splash
+    
     private let sessionManager: SessionManager
-
-    init(sessionManager: SessionManager) {
+    private var appSettings: AppSettingsProtocol
+    init(sessionManager: SessionManager, appSettings: AppSettingsProtocol = AppSettings.shared) {
         self.sessionManager = sessionManager
-        self.flow = sessionManager.state == .loggedIn ? .home : .auth
+        self.appSettings = appSettings
+      //  self.flow = sessionManager.state == .loggedIn ? .home : .auth
     }
-
+    
+    func splashDidFinish() {
+        if !appSettings.hasSeenOnboarding {
+            flow = .onboarding
+        } else if sessionManager.state == .loggedIn {
+            flow = .home
+        } else {
+            flow = .auth
+        }
+    }
+    
+    func completeOnboarding() {
+        appSettings.hasSeenOnboarding = true
+        flow = .auth
+    }
+    
     func startProfileSetup() {
         flow = .profileSetup //ProfileSetupCoordinator
     }
-
+    
     func startHomeFlow() {
         flow = .home //HomeCoordinator
     }
-
+    
     func goToProfileSetupDecision() {
         flow = .profileSetupDecision // oneScreen
     }
-
+    
     func startAuthFlow() {
         flow = .auth // AuthCoordinator
     }
-
+    
 }
